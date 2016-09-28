27d3aee9e0b0088e166b3cc43db5340a8e20718a
Louise Wilson <louisebrookswilson@gmail.com>
2016-09-28 13:38:33 -0400

    push

---
+++ main.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+
+
+  var  mn = $(".navFooter");
+      mns = "nav-footer-scrolled";
+      hdr = $('.hero').height();
+
+  $(window).scroll(function() {
+    if( $(this).scrollTop() > hdr ) {
+      mn.addClass(mns);
+    } else {
+      mn.removeClass(mns);
+    }
+  });
+
+
+
+
+
+  $(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
+
+
+
+ });
