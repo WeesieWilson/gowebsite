1e32db859e5b49fb11519b1b92dd0db3ab5894b4
Louise Wilson <louisebrookswilson@gmail.com>
2016-10-28 22:09:40 -0400

    azl

--- main.js
+++ main.js
@@ -17,20 +17,17 @@ $(document).ready(function() {
 
 
 
-  $(function() {
-    $('a[href*="#"]:not([href="#"])').click(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {
-          $('html, body').animate({
-            scrollTop: target.offset().top
-          }, 1000);
-          return false;
-        }
-      }
-    });
-  });
+    $( '#my-slider' ).sliderPro({
+
+      width: 960,
+    height: 400,
+    orientation: 'horizontal',
+    thumbnailPosition: 'right',
+    arrows: true,
+  }
+    );
+
+
 
 
 
